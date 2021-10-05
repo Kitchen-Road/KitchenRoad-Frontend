fd5af46de5ebfb86229ab3917fe7e3509daaeb23
brenno-silva <brenno.silva037@gmail.com>
2021-10-05 11:24:58 -0300

    Remoção da api

--- app/src/components/container/Login.js
+++ app/src/components/container/Login.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import Api from '../../api/GateAway';
+import Api from "../../api/GateAway";
 
 const Login = () => {
-    
-    const api = new Api();
-    const formInput = [
-        {
-            divClass : "sign-up",
-            type: "text", 
-            className: "inputUser", 
-            placeholder: "Digite seu nome:" 
-        }, 
-    ]
-    return (
-        <form>
-            <h1 className="title">Acessar conta</h1>
-            <div className="sign-up">
-                {/* <label>Email</label> */}
-                <input type="email" className="inputUser" placeholder="Digite seu email:" required/>
-            </div>
+  //const api = new Api();
+  const formInput = [
+    {
+      divClass: "sign-up",
+      type: "text",
+      className: "inputUser",
+      placeholder: "Digite seu nome:",
+    },
+  ];
+  return (
+    <form>
+      <h1 className="title">Acessar conta</h1>
+      <div className="sign-up">
+        {/* <label>Email</label> */}
+        <input
+          type="email"
+          className="inputUser"
+          placeholder="Digite seu email:"
+          required
+        />
+      </div>
 
-            <div className="sign-up">
-                {/* <label>Senha</label> */}
-                <input type="text" className="inputUser" placeholder="Digite sua senha:" required/>
-            </div>
+      <div className="sign-up">
+        {/* <label>Senha</label> */}
+        <input
+          type="text"
+          className="inputUser"
+          placeholder="Digite sua senha:"
+          required
+        />
+      </div>
 
-            <button className="button button-primary" onClick={() => api.cadastrar}>Acessar conta</button>
-            <p className="have-account">Esqueceu sua senha? Clique aqui</p>
-        </form>
-    )
-}
+      <button className="button button-primary">Acessar conta</button>
+      <p className="have-account">Esqueceu sua senha? Clique aqui</p>
+    </form>
+  );
+};
 
 export default Login;

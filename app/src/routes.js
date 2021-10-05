f8eae26b33817124cefceaf26293a128f4ee54a0
brenno-silva <brenno.silva037@gmail.com>
2021-10-05 10:46:23 -0300

    Adição da rota e correção da dark_green

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PageLogin from './pages/PageLogin';
-import PageRegister from './pages/PageRegister';
-import PageReceitas from './pages/PageReceitas';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageLogin from "./pages/PageLogin";
+import PageRegister from "./pages/PageRegister";
+import PageReceitas from "./pages/PageReceitas";
+import PageModoPreparo from "./pages/PageModoPreparo";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/SignUP" component={PageRegister} />
-                <Route path="/Login" component={PageLogin} />
-                <Route path="/Receitas" component={PageReceitas} />
-
-            </Switch>
-        </BrowserRouter>
-    );
-} 
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/SignUP" component={PageRegister} />
+        <Route path="/Login" component={PageLogin} />
+        <Route path="/Receitas" component={PageReceitas} />
+        <Route path="/ModoDePreparo" component={PageModoPreparo} />
+      </Switch>
+    </BrowserRouter>
+  );
+}

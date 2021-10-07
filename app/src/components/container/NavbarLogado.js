4348b7f68c8defd230d4d58d537c541d069c66db
DaviMatheus <matdavimat@gmail.com>
2021-10-07 04:27:10 -0700

    sair_para_home

--- app/src/components/container/NavbarLogado.js
+++ app/src/components/container/NavbarLogado.js
@@ -6,7 +6,7 @@ const NavbarLogado = () => {
     return (
         <div className="navbar">
         <p className="label-nav"> <img src={frigideira}/> vamos cozinhar</p>
-        <Link to="/Login"><button className="btn-nav">Sair</button></Link>
+        <Link to="/"><button className="btn-nav">Sair</button></Link>
         </div>
     )
 }

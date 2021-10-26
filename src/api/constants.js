ef846ba2bd7f9ec92c782332284b7caadda1fe52
brenno-silva <brenno.silva037@gmail.com>
2021-10-26 20:06:11 -0300

    Adição da página de receitas concluidas

--- src/api/constants.js
+++ src/api/constants.js
@@ -9,6 +9,7 @@ const API = {
   DIFICULDADE: "&dificuldade=",
   DICA: "dicas/",
   CONCLUIR: "concluir-receita/",
+  LISTCONCLUIDAS: "listar-concluidas/",
   TOKEN: "524ead71841625521f2abb80f5ceb5e475bd7e3ad8cc61dedf22466894900f25",
 };
 
--- src/components/container/NavbarLogado.js
+++ src/components/container/NavbarLogado.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import frigideira from '../../assets/img/modoPreparo/frigideira.png'
+import frigideira from "../../assets/img/modoPreparo/frigideira.png";
+import kitchen from "../../assets/img/kitchen.png";
 
 const NavbarLogado = () => {
-    const handleClick = () => {
-        localStorage.removeItem('receitas_concluidas');
-    };
-    
-    return (
-        <div className="navbar">
-        <p className="label-nav"> <img src={frigideira}/> vamos cozinhar</p>
-        <Link to="/"><button className="btn-nav"onClick={() => handleClick()}>Sair</button></Link>
-        </div>
-    )
-}
+  const handleClick = () => {
+    localStorage.removeItem("receitas_concluidas");
+  };
+
+  return (
+    <div className="navbar">
+      <Link to="/receitas">
+        <p className="label-nav">
+          <img src={frigideira} /> vamos cozinhar
+        </p>
+      </Link>
+      <Link to="/receitasConcluidas">
+        <img className="img-pad" src={kitchen} width="50" height="40"></img>
+      </Link>
+      <Link to="/">
+        <button className="btn-nav" onClick={() => handleClick()}>
+          Sair
+        </button>
+      </Link>
+    </div>
+  );
+};
 
 export default NavbarLogado;

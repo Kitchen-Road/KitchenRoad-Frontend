f68eff10de964d2c0879734c9cf52568df8f5ba3
Natanael Filho <filhonatanael01@gmail.com>
2021-10-26 12:00:00 -0300

    add conquistas

--- src/pages/PageModoPreparo.js
+++ src/pages/PageModoPreparo.js
@@ -8,6 +8,7 @@ import panelaReceita from '../assets/img/modoPreparo/panela-receita.png'
 import panelaModoPreparo from '../assets/img/modoPreparo/panela-modo-preparo.png'
 import conchaIngredientes from '../assets/img/modoPreparo/concha-ingredientes.png'
 import Conquista from "../components/utils/Conquista.js";
+import PrimeiraReceita from '../assets/img/conquista/primeira-receita.png'
 
 import useSound from 'use-sound';
 import trompete from '../assets/music/trompete.mp3'
@@ -28,13 +29,44 @@ function formatExperienciaReceita(dificuldade) {
   return xp;
 }
 
+function getNome(id){
+  return id === '1' ? 'Primeira de muitas' :
+         id === '5' ? 'Quase profissional':
+         id === '10' ? 'Masterchef':
+         id === '15' ? 'Melhor que o Jacquin Barriga':
+         ''
+}
+
+function getImg(id){
+  return id === '1' ? PrimeiraReceita:
+         id === '5' ? 'Quase profissional':
+         id === '10' ? 'https://www.puccicozinhas.com.br/wp-content/uploads/2017/05/Cozinha_Pucci_Slide_Cozinheira_Profissional.png':
+         id === '15' ? 'https://play-lh.googleusercontent.com/zICISZPXRhNwnGcIVscVoB9eVlLH8KWdki7rTCSMesg3dDOG3zf_66_oWKhKPHfnN5Q4':
+         ''
+}
+function getConquistaPorQuantidade(qnt){
+  let nome = getNome(qnt)
+  let img = getImg(qnt)
+  const resultado = {
+    nome_conquista: nome,
+    imagem: img,
+    descrição_conquista: 'Está conquista é adquirida ao completar ' + qnt + ' receitas',   
+  }
+  return resultado
+}
+
 function PageModoPreparo() {
   const { id } = useParams();
   const [play] = useSound(
     trompete,
     { volume: 0.5 }
   );
-
+  const [conquistaAdquirida, setConquistaAdquirida] = useState({
+    nome_conquista: '-------',
+    imagem: 'https://cdn.discordapp.com/attachments/869650300913479715/902398054852874290/rei-do-alho-logo-5ED449F072-seeklogo.png',
+    descrição_conquista: "------------------",
+    
+  })
   const [receita, setReceita] = useState({
     nome_receita: "-----",
     categoria: "-----",
@@ -58,15 +90,19 @@ function PageModoPreparo() {
     if(localStorage.getItem('receitas_concluidas') === null || localStorage.getItem('receitas_concluidas') === '' || localStorage.getItem('receitas_concluidas') === 'NaN')
       localStorage.setItem('receitas_concluidas', 0)
     localStorage.setItem('receitas_concluidas', parseInt(localStorage.getItem('receitas_concluidas'))+1)
-    if(parseInt(localStorage.getItem('receitas_concluidas')) === 1 ||
-       parseInt(localStorage.getItem('receitas_concluidas')) === 5 ||
-       parseInt(localStorage.getItem('receitas_concluidas')) === 10 ||
-       parseInt(localStorage.getItem('receitas_concluidas')) === 15
+    let receitas_concluidas = localStorage.getItem('receitas_concluidas')
+    if( receitas_concluidas  === '1' ||
+        receitas_concluidas  === '5' ||
+        receitas_concluidas  === '10' ||
+        receitas_concluidas  === '15'
       ){
+      const resultado = getConquistaPorQuantidade(receitas_concluidas)
+      setConquistaAdquirida(resultado)
       play()
       return true
     }
     return false
+      
   };
 
   return (
@@ -101,13 +137,13 @@ function PageModoPreparo() {
             onClick={() => setButtonConquista(handleClick())}>
             Marcar como concluída
           </button>
-      <Link to="/Receitas">
+      {/* <Link to="/Receitas"> */}
         <Conquista trigger={buttonConquista} setTrigger={setButtonConquista}>
-          <h2 className="title-1">Rei do Alho</h2>
-          <img src="https://cdn.discordapp.com/attachments/869650300913479715/902398054852874290/rei-do-alho-logo-5ED449F072-seeklogo.png" alt="Erro para "/>
-          <p>Lorem Lorem ipsum e isso</p>
+          <h2 className="title-1">{conquistaAdquirida.nome_conquista}</h2>
+          <img src={conquistaAdquirida.imagem} alt="Erro para carregar a imagem "/>
+          <p>{conquistaAdquirida.descrição_conquista}</p>
         </Conquista>
-      </Link>
+      {/* </Link> */}
     </div>
   );
 }

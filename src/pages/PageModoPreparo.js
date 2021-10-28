2f5c10ca718e655c909919d8c3b24af51407f732
brenno-silva <brenno.silva037@gmail.com>
2021-10-28 16:17:37 -0300

    Correção na conclusão de receitas

--- src/pages/PageModoPreparo.js
+++ src/pages/PageModoPreparo.js
@@ -9,28 +9,28 @@ import panelaReceita from "../assets/img/modoPreparo/panela-receita.png";
 import panelaModoPreparo from "../assets/img/modoPreparo/panela-modo-preparo.png";
 import conchaIngredientes from "../assets/img/modoPreparo/concha-ingredientes.png";
 import Conquista from "../components/utils/Conquista.js";
-import useSound from 'use-sound';
+import useSound from "use-sound";
 
-import trompete from '../assets/music/trompete.mp3'
-import demon from '../assets/music/demon.mp3'
-import churras_musica from '../assets/music/churras_musica.mp3'
-import musica_casamento from '../assets/music/casamento.mp3'
-import musica_epoca from '../assets/music/grande-familia.mp3'
-import musica_natal from '../assets/music/musica_natal.mp3'
-import musica_rei_alho from '../assets/music/rei_alho.mp3'
-import musica_halloween from '../assets/music/halloween.mp3'
+import trompete from "../assets/music/trompete.mp3";
+import demon from "../assets/music/demon.mp3";
+import churras_musica from "../assets/music/churras_musica.mp3";
+import musica_casamento from "../assets/music/casamento.mp3";
+import musica_epoca from "../assets/music/grande-familia.mp3";
+import musica_natal from "../assets/music/musica_natal.mp3";
+import musica_rei_alho from "../assets/music/rei_alho.mp3";
+import musica_halloween from "../assets/music/halloween.mp3";
 
-import PrimeiraReceita from '../assets/img/conquista/primeira-receita.png'
-import HellsKitchen from "../assets/img/conquista/HellsKitchen.png"
-import pengu from "../assets/img/conquista/pingu.png"
-import velho from "../assets/img/conquista/velhoputo.png"
-import jackin from "../assets/img/conquista/jackin.png"
-import Anel from "../assets/img/conquista/anel.png"
-import Carne from "../assets/img/conquista/carne.png"
-import EpocaImg from "../assets/img/conquista/beicola.png"
-import rei_alho from "../assets/img/conquista/rei_alho.png"
-import PaveImg from "../assets/img/conquista/pave.png"
-import HalloweenImg from '../assets/img/conquista/halloween.png'
+import PrimeiraReceita from "../assets/img/conquista/primeira-receita.png";
+import HellsKitchen from "../assets/img/conquista/HellsKitchen.png";
+import pengu from "../assets/img/conquista/pingu.png";
+import velho from "../assets/img/conquista/velhoputo.png";
+import jackin from "../assets/img/conquista/jackin.png";
+import Anel from "../assets/img/conquista/anel.png";
+import Carne from "../assets/img/conquista/carne.png";
+import EpocaImg from "../assets/img/conquista/beicola.png";
+import rei_alho from "../assets/img/conquista/rei_alho.png";
+import PaveImg from "../assets/img/conquista/pave.png";
+import HalloweenImg from "../assets/img/conquista/halloween.png";
 
 function formatExperienciaReceita(dificuldade) {
   var xp;
@@ -49,126 +49,141 @@ function formatExperienciaReceita(dificuldade) {
 }
 // Verifca mascara
 
-function startLocalStorage(str){
-  if(localStorage.getItem(str) === null || 
-     localStorage.getItem(str) === '' || 
-     localStorage.getItem(str) === 'NaN')
-     localStorage.setItem(str, 0)
+function startLocalStorage(str) {
+  if (
+    localStorage.getItem(str) === null ||
+    localStorage.getItem(str) === "" ||
+    localStorage.getItem(str) === "NaN"
+  )
+    localStorage.setItem(str, 0);
 }
 
-function verificaExist(){
+function verificaExist() {
   //receitas_concluidas
-  startLocalStorage('receitas_concluidas')
+  startLocalStorage("receitas_concluidas");
   // epoca_ano
-  startLocalStorage('epoca_ano')
+  startLocalStorage("epoca_ano");
   //churrasco
-  startLocalStorage('churrasco')
+  startLocalStorage("churrasco");
   // alho
-  startLocalStorage('alho')
+  startLocalStorage("alho");
   // casamento
-  startLocalStorage('casamento')
+  startLocalStorage("casamento");
   //natal
-  startLocalStorage('natal')
+  startLocalStorage("natal");
   //halloween
-  startLocalStorage('halloween')  
+  startLocalStorage("halloween");
 }
 
-function ehConquistaQNT(){
-  localStorage.setItem('receitas_concluidas', parseInt(localStorage.getItem('receitas_concluidas'))+1)
-  let receitas_concluidas = localStorage.getItem('receitas_concluidas')  
-  if( receitas_concluidas  === '1' ||
-      receitas_concluidas  === '5' ||
-      receitas_concluidas  === '10' ||
-      receitas_concluidas  === '15' ||
-      receitas_concluidas === '20'
-       
-    ){
-    return true
+function ehConquistaQNT() {
+  localStorage.setItem(
+    "receitas_concluidas",
+    parseInt(localStorage.getItem("receitas_concluidas")) + 1
+  );
+  let receitas_concluidas = localStorage.getItem("receitas_concluidas");
+  if (
+    receitas_concluidas === "1" ||
+    receitas_concluidas === "5" ||
+    receitas_concluidas === "10" ||
+    receitas_concluidas === "15" ||
+    receitas_concluidas === "20"
+  ) {
+    return true;
   }
-  return false
+  return false;
 }
 
-function ehCasamento(id){
-  let casamento = localStorage.getItem('casamento')
-  if( id === '21'  && casamento !== '1'){
-    localStorage.setItem('casamento', 1)
-    return true
+function ehCasamento(id) {
+  let casamento = localStorage.getItem("casamento");
+  if (id === "21" && casamento !== "1") {
+    localStorage.setItem("casamento", 1);
+    return true;
   }
-  return false
+  return false;
 }
 
-function ehChurrasco(id){
-  if(id === '12' ||
-     id === '13' ||
-     id === '14' )
-    localStorage.setItem('churrasco' , parseInt(localStorage.getItem('churrasco'))+1 )
-  let churrasco = localStorage.getItem('churrasco')
-  if(churrasco === '3'){
-    return true
+function ehChurrasco(id) {
+  if (id === "12" || id === "13" || id === "14")
+    localStorage.setItem(
+      "churrasco",
+      parseInt(localStorage.getItem("churrasco")) + 1
+    );
+  let churrasco = localStorage.getItem("churrasco");
+  if (churrasco === "3") {
+    return true;
   }
-    
-  return false
+
+  return false;
 }
 
-function ehEpocaAno(id){
-  if(id === '3' ||
-     id === '7' ||
-     id === '6' ||
-     id === '8' ||
-     id === '11' ||
-     id === '10' )
-    localStorage.setItem('epoca_ano' , parseInt(localStorage.getItem('epoca_ano'))+1 )
-  let epoca_ano = localStorage.getItem('epoca_ano')
-  if(epoca_ano === '6'){
-    return true
+function ehEpocaAno(id) {
+  if (
+    id === "3" ||
+    id === "7" ||
+    id === "6" ||
+    id === "8" ||
+    id === "11" ||
+    id === "10"
+  )
+    localStorage.setItem(
+      "epoca_ano",
+      parseInt(localStorage.getItem("epoca_ano")) + 1
+    );
+  let epoca_ano = localStorage.getItem("epoca_ano");
+  if (epoca_ano === "6") {
+    return true;
   }
-    
-  return false
+
+  return false;
 }
-function ehReiDoAlho(id){
-  if(id === '9' ||
-     id === '12' ||
-     id === '18' )
-    localStorage.setItem('alho' , parseInt(localStorage.getItem('alho'))+1 )
-  let epoca_ano = localStorage.getItem('alho')
-  if(epoca_ano === '3'){
-    return true
+function ehReiDoAlho(id) {
+  if (id === "9" || id === "12" || id === "18")
+    localStorage.setItem("alho", parseInt(localStorage.getItem("alho")) + 1);
+  let epoca_ano = localStorage.getItem("alho");
+  if (epoca_ano === "3") {
+    return true;
   }
-    
-  return false
+
+  return false;
 }
 
-function ehNatal(id){
-  if(id === '3' ||
-     id === '7' )
-    localStorage.setItem('natal' , parseInt(localStorage.getItem('natal'))+1 )
-  let natal = localStorage.getItem('natal')
-  if(natal === '2'){
-    return true
+function ehNatal(id) {
+  if (id === "3" || id === "7")
+    localStorage.setItem("natal", parseInt(localStorage.getItem("natal")) + 1);
+  let natal = localStorage.getItem("natal");
+  if (natal === "2") {
+    return true;
   }
-    
-  return false
+
+  return false;
 }
 
-function ehHalloween(id){
-  if(id === '10' ||
-     id === '11' )
-    localStorage.setItem('halloween' , parseInt(localStorage.getItem('halloween'))+1 )
-  let halloween = localStorage.getItem('halloween')
-  if(halloween === '2'){
-    return true
+function ehHalloween(id) {
+  if (id === "10" || id === "11")
+    localStorage.setItem(
+      "halloween",
+      parseInt(localStorage.getItem("halloween")) + 1
+    );
+  let halloween = localStorage.getItem("halloween");
+  if (halloween === "2") {
+    return true;
   }
-    
-  return false
+
+  return false;
 }
 // Conquista QNT
-function getNome(id){
-  return id === '1' ? 'Primeira de muitas' :
-         id === '5' ? 'Quase profissional':
-         id === '10' ? 'Masterchef':
-         id === '15' ? 'Melhor que o Jacquin Barriga':
-         id === '20' ? 'Cozinheiro do Inferno': 
-         ''
+function getNome(id) {
+  return id === "1"
+    ? "Primeira de muitas"
+    : id === "5"
+    ? "Quase profissional"
+    : id === "10"
+    ? "Masterchef"
+    : id === "15"
+    ? "Melhor que o Jacquin Barriga"
+    : id === "20"
+    ? "Cozinheiro do Inferno"
+    : "";
 }
 
 function getImg(id) {
@@ -185,10 +200,10 @@ function getImg(id) {
     : "";
 }
 
-function getConquistaPorQuantidade(){
-  let qnt = localStorage.getItem('receitas_concluidas')
-  let nome = getNome(qnt)
-  let img = getImg(qnt)
+function getConquistaPorQuantidade() {
+  let qnt = localStorage.getItem("receitas_concluidas");
+  let nome = getNome(qnt);
+  let img = getImg(qnt);
 
   const resultado = {
     nome_conquista: nome,
@@ -201,100 +216,78 @@ function getConquistaPorQuantidade(){
 
 // Conquista Categoria
 
-function getCasamento(){
+function getCasamento() {
   const casamento = {
-    nome_conquista: 'Felizes para sempre',
+    nome_conquista: "Felizes para sempre",
     imagem: Anel,
-    descrição_conquista: 'Está conquista é adquirida ao se completar o bolo de casamento',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar o bolo de casamento",
+  };
+  return casamento;
 }
 
-function getChurrasco(){
+function getChurrasco() {
   const casamento = {
-    nome_conquista: 'Só falta a cerveja',
+    nome_conquista: "Só falta a cerveja",
     imagem: Carne,
-    descrição_conquista: 'Está conquista é adquirida ao se completar todas as receitas para churrasco',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar todas as receitas para churrasco",
+  };
+  return casamento;
 }
 
-
-function getNatal(){
+function getNatal() {
   const casamento = {
-    nome_conquista: 'é pavê ou pá come',
+    nome_conquista: "é pavê ou pá come",
     imagem: PaveImg,
-    descrição_conquista: 'Está conquista é adquirida ao se completar todas as receitas de natal',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar todas as receitas de natal",
+  };
+  return casamento;
 }
 
-function getHalloween(){
+function getHalloween() {
   const casamento = {
     nome_conquista: "Que os jogos comecem",
     imagem: HalloweenImg,
-    descrição_conquista: 'Está conquista é adquirida ao se completar todas as receitas de halloween',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar todas as receitas de halloween",
+  };
+  return casamento;
 }
 
-
-function getEpocaAno(){
+function getEpocaAno() {
   const casamento = {
-    nome_conquista: 'A grande família',
+    nome_conquista: "A grande família",
     imagem: EpocaImg,
-    descrição_conquista: 'Está conquista é adquirida ao se completar todas as receitas de época',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar todas as receitas de época",
+  };
+  return casamento;
 }
 
-function getAlho(){
+function getAlho() {
   const casamento = {
-    nome_conquista: 'Rei do Alho',
+    nome_conquista: "Rei do Alho",
     imagem: rei_alho,
-    descrição_conquista: 'Está conquista é adquirida ao se completar todas as receitas que contém alho',
-  }
-  return casamento
+    descrição_conquista:
+      "Está conquista é adquirida ao se completar todas as receitas que contém alho",
+  };
+  return casamento;
 }
-
-
 
 function PageModoPreparo() {
   const { id } = useParams();
 
-  const [play] = useSound(
-    trompete,
-    { volume: 0.5 }
-  );
-  const [play_churrasco] = useSound(
-    churras_musica,
-    { volume: 0.2 }
-  );
-  const [play_demon] = useSound(
-    demon,
-    { volume: 0.5 }
-  );
-  const [play_epoca] = useSound(
-    musica_epoca,
-    { volume: 0.5 }
-  );
-  const [play_casamento] = useSound(
-    musica_casamento,
-    { volume: 0.2 }
-  );
-  const [play_natal] = useSound(
-    musica_natal,
-    { volume: 0.2 }
-  );
-  const [play_alho] = useSound(
-    musica_rei_alho,
-    { volume: 0.2 }
-  );
-  const [play_halloween] = useSound(
-    musica_halloween,
-    { volume: 0.4 }
-  );
-  
+  const [play] = useSound(trompete, { volume: 0.5 });
+  const [play_churrasco] = useSound(churras_musica, { volume: 0.2 });
+  const [play_demon] = useSound(demon, { volume: 0.5 });
+  const [play_epoca] = useSound(musica_epoca, { volume: 0.5 });
+  const [play_casamento] = useSound(musica_casamento, { volume: 0.2 });
+  const [play_natal] = useSound(musica_natal, { volume: 0.2 });
+  const [play_alho] = useSound(musica_rei_alho, { volume: 0.2 });
+  const [play_halloween] = useSound(musica_halloween, { volume: 0.4 });
+
   const [conquistaAdquirida, setConquistaAdquirida] = useState({
     nome_conquista: "-------",
     imagem:
@@ -321,89 +314,104 @@ function PageModoPreparo() {
     loadModoPreparo();
   }, []);
 
+  const loadConclusaoResponse = async () => {
+    let response;
+    response = await concluirPOST(receita.nome_receita);
+    if (response.status == 200) setButtonValid(true);
+  };
+
   function escolhe_musica() {
     // rei alho
-    if(localStorage.getItem('alho') === '3'){
-      localStorage.setItem('alho' , parseInt(localStorage.getItem('alho'))+1 )
-      play_alho()
+    if (localStorage.getItem("alho") === "3") {
+      localStorage.setItem("alho", parseInt(localStorage.getItem("alho")) + 1);
+      play_alho();
     }
     // casamento
-    else if(localStorage.getItem('casamento') === '1'){
-      localStorage.setItem('casamento', 2)
-      play_casamento()  
+    else if (localStorage.getItem("casamento") === "1") {
+      localStorage.setItem("casamento", 2);
+      play_casamento();
     }
     // grande familia
-    else if(localStorage.getItem('epoca_ano') === '6'){
-      localStorage.setItem('epoca_ano' , parseInt(localStorage.getItem('epoca_ano'))+1 )
-      play_epoca()  
+    else if (localStorage.getItem("epoca_ano") === "6") {
+      localStorage.setItem(
+        "epoca_ano",
+        parseInt(localStorage.getItem("epoca_ano")) + 1
+      );
+      play_epoca();
     }
     // pave pa come
-    else if(localStorage.getItem('natal') === '2' ){
-      localStorage.setItem('natal' , parseInt(localStorage.getItem('natal'))+1 )
-      play_natal()
+    else if (localStorage.getItem("natal") === "2") {
+      localStorage.setItem(
+        "natal",
+        parseInt(localStorage.getItem("natal")) + 1
+      );
+      play_natal();
     }
     // halloween
-    else if(localStorage.getItem('halloween') === '2'){
-      localStorage.setItem('halloween' , parseInt(localStorage.getItem('halloween'))+1 )
-      play_halloween()  
+    else if (localStorage.getItem("halloween") === "2") {
+      localStorage.setItem(
+        "halloween",
+        parseInt(localStorage.getItem("halloween")) + 1
+      );
+      play_halloween();
     }
     // churrasco
-    else if(localStorage.getItem('churrasco') === '3'){
-      localStorage.setItem('churrasco' , parseInt(localStorage.getItem('churrasco'))+1 )
-      play_churrasco()  
+    else if (localStorage.getItem("churrasco") === "3") {
+      localStorage.setItem(
+        "churrasco",
+        parseInt(localStorage.getItem("churrasco")) + 1
+      );
+      play_churrasco();
     }
     // demon
-    else if(localStorage.getItem('receitas_concluidas')  === '20' ){
-      play_demon()  
+    else if (localStorage.getItem("receitas_concluidas") === "20") {
+      play_demon();
     }
-    // padrao    
-    else
-      play()
+    // padrao
+    else play();
   }
-  
+
   const handleClick = () => {
-    verificaExist()
-    var resultado = false
-    if(ehConquistaQNT()){
-      setConquistaAdquirida(getConquistaPorQuantidade())
-      resultado = true
+    verificaExist();
+    loadConclusaoResponse();
+    var resultado = false;
+    if (ehConquistaQNT()) {
+      setConquistaAdquirida(getConquistaPorQuantidade());
+      resultado = true;
     }
 
-    if(ehNatal(id)){
-      setConquistaAdquirida(getNatal())
-      resultado = true
+    if (ehNatal(id)) {
+      setConquistaAdquirida(getNatal());
+      resultado = true;
     }
 
-    if(ehHalloween(id)){
-      setConquistaAdquirida(getHalloween())
-      resultado = true
+    if (ehHalloween(id)) {
+      setConquistaAdquirida(getHalloween());
+      resultado = true;
     }
 
-
-    if(ehEpocaAno(id)){
-      setConquistaAdquirida(getEpocaAno())
-      resultado = true
+    if (ehEpocaAno(id)) {
+      setConquistaAdquirida(getEpocaAno());
+      resultado = true;
     }
 
-    if(ehChurrasco(id)){
-      setConquistaAdquirida(getChurrasco())
-      resultado = true
+    if (ehChurrasco(id)) {
+      setConquistaAdquirida(getChurrasco());
+      resultado = true;
     }
 
-    if (ehCasamento(id)){
-      setConquistaAdquirida(getCasamento())
-      resultado = true
+    if (ehCasamento(id)) {
+      setConquistaAdquirida(getCasamento());
+      resultado = true;
     }
-    
-    if (ehReiDoAlho(id)){
-      setConquistaAdquirida(getAlho())
-      resultado = true
+
+    if (ehReiDoAlho(id)) {
+      setConquistaAdquirida(getAlho());
+      resultado = true;
     }
-    if(resultado)
-      escolhe_musica()  
+    if (resultado) escolhe_musica();
 
-    return resultado 
-
+    return resultado;
   };
 
   return (
